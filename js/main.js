ca354f33ca3ce27de586e45d018271e038931809
rinox3005 <peppe.rinox@gmail.com>
2024-05-22 16:34:18 +0200

    [UPD] milestone 1 done

--- js/main.js
+++ js/main.js
@@ -8,9 +8,22 @@ createApp({
       todoList: [
         {
           text: "Buy a new shirt",
-          done: true,
+          done: false,
+        },
+        {
+          text: "Buy pet food",
+          done: false,
+        },
+        {
+          text: "Buy oranges",
+          done: false,
+        },
+        {
+          text: "Call John",
+          done: false,
         },
       ],
     };
   },
+  methods: {},
 }).mount("#app");
